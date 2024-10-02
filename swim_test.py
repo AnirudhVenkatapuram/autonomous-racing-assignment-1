dedebeffbfc52c6b47f20505f080a751e16a033a
ani <qzf7nj@virginia.edu>
2024-10-02 13:28:44 -0400

    trying out based on position

--- swim_test.py
+++ swim_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
-import math
+from turtlesim.msg import Pose
 
 class FigureEightMover:
     def __init__(self):
@@ -11,43 +11,59 @@ class FigureEightMover:
         # Define a publisher to the /turtle1/cmd_vel topic
         self.cmd_vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
-        # High precision value of Pi
-        pi = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196
+        # Create a subscriber to the /turtle1/pose topic to get the turtle's position
+        rospy.Subscriber('/turtle1/pose', Pose, self.pose_callback)
 
-        # Set linear and initial angular velocities
-        linear_velocity = 2.0
-        angular_velocity = 1.0
-
-        # Time required to complete one full circle using high precision pi
-        self.T = 2 * pi / angular_velocity
+        # Initialize position variables
+        self.turtle_pose = Pose()
 
         # Create the Twist message to control the turtle
         self.move_cmd = Twist()
-        self.move_cmd.linear.x = linear_velocity
-        self.move_cmd.angular.z = angular_velocity
 
-        # Set initial variables
-        self.circles_completed = 0
+        # Set initial linear and angular velocities
+        self.move_cmd.linear.x = 2.0
+        self.move_cmd.angular.z = 1.0
 
-        # Timer to control direction switching
-        rospy.Timer(rospy.Duration(self.T), self.switch_direction)
+        # Set flags to track which part of the figure-eight we're in
+        self.switch_to_bottom_circle = False
+        self.switch_to_top_circle = False
 
+        # Start the turtle movement
         rospy.loginfo("Starting figure-eight movement...")
 
-        # Keep the turtle moving indefinitely
+        # Start the loop to keep moving the turtle
         self.keep_moving()
 
-    def switch_direction(self, event):
-        """ Switches the direction of angular velocity to create the figure-eight pattern. """
-        # Switch angular velocity to reverse direction
-        self.move_cmd.angular.z = -self.move_cmd.angular.z
-        self.circles_completed += 1
-        rospy.loginfo(f"Switching direction. Circles completed: {self.circles_completed}")
+    def pose_callback(self, data):
+        """ Callback function to get the current position of the turtle. """
+        self.turtle_pose = data
+
+        # Check for position thresholds to switch direction
+        self.check_position_and_switch()
+
+    def check_position_and_switch(self):
+        """ Check the turtle's position and switch direction if necessary. """
+        # Get the current position of the turtle
+        x = self.turtle_pose.x
+        y = self.turtle_pose.y
+
+        # If the turtle is at the top of the figure-eight (near y=10), switch to bottom circle
+        if y >= 10.5 and not self.switch_to_bottom_circle:
+            self.move_cmd.angular.z = -self.move_cmd.angular.z
+            self.switch_to_bottom_circle = True
+            self.switch_to_top_circle = False
+            rospy.loginfo("Switched to bottom circle. Current position: x={}, y={}".format(x, y))
+
+        # If the turtle is at the bottom of the figure-eight (near y=2), switch to top circle
+        if y <= 1.5 and not self.switch_to_top_circle:
+            self.move_cmd.angular.z = -self.move_cmd.angular.z
+            self.switch_to_top_circle = True
+            self.switch_to_bottom_circle = False
+            rospy.loginfo("Switched to top circle. Current position: x={}, y={}".format(x, y))
 
     def keep_moving(self):
-        """ Publishes the current movement command indefinitely. """
-        rate = rospy.Rate(1000)  # 20 Hz rate
-
+        """ Keep publishing the move command indefinitely. """
+        rate = rospy.Rate(100)  # Increase rate to 100 Hz for smoother control
         while not rospy.is_shutdown():
             # Publish the move command to make the turtle move
             self.cmd_vel.publish(self.move_cmd)
